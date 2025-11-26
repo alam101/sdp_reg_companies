7e469b8c193ca7a89a8ee723224474bff85b9cf3
fakhare alam <fakhre.alam101@in.ibm.com>
2025-11-26 16:03:55 +0530

    fix

--- src/app/newBoarding/new-diet/new-diet.page.ts
+++ src/app/newBoarding/new-diet/new-diet.page.ts
@@ -665,7 +665,6 @@ export class NewDietPage implements OnInit,AfterViewInit,OnDestroy {
   }
   isdoenloadclicked=false;
   async gotoDownloadPopup(){ 
-     debugger;
      if(this.clientId==='alyve'){
       this.design='new';
      this.isdoenloadclicked=true;
@@ -676,12 +675,10 @@ export class NewDietPage implements OnInit,AfterViewInit,OnDestroy {
       this.isdoenloadclicked=true;
       localStorage.setItem("company_id","wellbeing");
       this.design==='old'?this.downloadPdfFromApi(): this.downloadPdfFromApiNew();
-     }
-     
+     }     
      else if(this.clientId==='redcliffe'){
-      debugger;
       this.design='new';
-      this.response_type="file";
+      this.response_type="url";
       this.isdoenloadclicked=true;
       localStorage.setItem("company_id","RedcliffeLabs");
       if(this.response_type==="file"){
