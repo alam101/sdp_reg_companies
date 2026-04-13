5012931db44f12498d5bc0a8e0f6e7c4c7350425
System Administrator <root@fakhares-MacBook-Air-2.local>
2026-04-13 18:49:25 +0530

    fix wellbeing

--- src/app/newBoarding/Components/meal-workout/meal-workout.page.ts
+++ src/app/newBoarding/Components/meal-workout/meal-workout.page.ts
@@ -11,7 +11,7 @@ import { times } from "lodash";
 import moment from "moment";
 import { empty } from "rxjs";
 import { AppService } from "../../../home-service/app.service";
-import { CONSTANTS } from "src/app/core/constants/constants";
+import { CONSTANTS, WELLBEING_UTM_MAP, WELLBEING_UTM_SOURCE } from "src/app/core/constants/constants";
 import { UTILITIES } from "src/app/core/utility/utilities";
 import { PortionCountPage } from "../../Components/alternate-diet/portion-count/portion-count.page";
 import { ViewProductPage } from "../../Components/view-product/view-product.page";
@@ -153,82 +153,13 @@ export class MealWorkoutPage implements OnInit {
     let url = d.BuyUrl;
     if (this.clientId === "wellbeing") {
       const foodId = String(d?.itemCode ?? d?.code ?? d?._id ?? "");
-      const utmMedium = this.getUtmMedium(foodId);
+      const utmMedium = WELLBEING_UTM_MAP[foodId];
       if (utmMedium) {
         const separator = url.includes("?") ? "&" : "?";
-        url = `${url}${separator}utm_source=FTRFY_dtplan_wa&utm_medium=${utmMedium}`;
+        url = `${url}${separator}utm_source=${WELLBEING_UTM_SOURCE}&utm_medium=${utmMedium}`;
       }
     }
     window.open(url, '_blank');
-  }
-
-  getUtmMedium(foodId: string): string {
-    const utmMap: { [key: string]: string } = {
-      "3451": "korean_marine_collagen",
-      "3457": "glow_korean_marine_collagen_tropical_bliss",
-      "3458": "glow_korean_marine_collagen_pinacolada",
-      "3463": "beauty_korean_marine_collagen_mango_peach",
-      "3462": "beauty_korean_marine_collagen_strawberry_watermelon",
-      "3460": "vegan_pro_collagen_unflavored",
-      "3459": "vegan_pro_collagen_berry_blast",
-      "3453": "skin_fuel_efferverscent_tablets",
-      "4077": "daily_greens_efferverscent_tablets",
-      "4076": "probiotics_prebiotics_efferverscent_tablets",
-      "3424": "apple_cider_vinegar_with_garcinia_efferverscent_tablets",
-      "4078": "grandmas_kadha_efferverscent_tablets",
-      "4079": "organic_vitamin_c_zinc_efferverscent_tablets",
-      "3423": "daily_fiber_pinacolada",
-      "3422": "daily_fiber_vanilla_berry",
-      "4080": "sleep_adaptogenic_herbal_tea",
-      "3421": "slim_adaptogenic_herbal_tea",
-      "3448": "her_care_adaptogenic_herbal_tea",
-      "4068": "melts_restful_sleep_10mg",
-      "4070": "melts_restful_sleep_5mg",
-      "4075": "melts_throat_relief",
-      "4069": "melts_vegan_vitamin_b12",
-      "3461": "melts_healthy_hair",
-      "4072": "melts_healthy_gut",
-      "3447": "melts_natural_vitamin_d3",
-      "4071": "melts_multivitamins",
-      "3446": "melts_vital_iron",
-      "4073": "melts_instant_energy",
-      "3449": "melts_hair_fall_control",
-      "4067": "melts_testo_power",
-      "4074": "melts_calm_relaxation",
-      "3455": "skin_fuel_pro_valencia_orange",
-      "3454": "skin_fuel_pro_strawberry",
-      "3428": "superfood_plant_protein_belgian_chocolate",
-      "3432": "vegan_protein_belgian_dark_chocolate",
-      "3431": "vegan_protein_french_vanilla_caramel",
-      "3427": "superfood_plant_protein_british_banoffee_pie",
-      "3430": "vegan_protein_canadian_mixed_berry",
-      "3429": "her_superfood_plant_protein_chocolate_peanut_butter",
-      "4081": "shilajit_gold",
-      "4082": "shilajit_resin",
-      "3442": "slow_multi_omega_for_her",
-      "3441": "slow_multi_omega_for_him",
-      "3452": "slow_multi_omega_for_50plus",
-      "4066": "slow_liver_detox",
-      "3418": "slow_triple_strength_omega_3",
-      "3440": "slow_triple_magnesium_complex",
-      "3420": "slow_daily_probiotics",
-      "3419": "slow_slow_burn",
-      "3456": "slow_hair_skin_nails",
-      "3443": "slow_diabetes_care",
-      "3444": "slow_pcos_balance",
-      "3450": "slow_bone_support",
-      "3425": "organic_apple_cider_vinegar",
-      "3426": "organic_apple_cider_vinegar_amla_turmeric",
-      "3437": "whey_protein_isolate_unflavored",
-      "3436": "whey_protein_isolate_dark_chocolate",
-      "3434": "whey_protein_blend_cappuccino",
-      "3433": "whey_protein_blend_swiss_chocolate",
-      "3435": "whey_protein_concentrate_unflavored",
-      "3439": "creatine_plus_unflavored",
-      "3438": "creatine_monohydrate_unflavored",
-      "4083": "hangover_relief",
-    };
-    return utmMap[foodId] || "";
   }
   videoClick(data) {
     this.videoUrl = this._sanitizer.bypassSecurityTrustResourceUrl(data);
